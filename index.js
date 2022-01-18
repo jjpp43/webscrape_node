6720fd4598726f6b80dc27d2a19875684687aaad
jjpp43 <junnapark@gmail.com>
2022-01-18 11:40:17 +0000

    Successfully scraped information about bank, url, interest. From now on, need to work on how to loop through tr

--- index.js
+++ index.js
@@ -5,13 +5,39 @@ const express = require('express')
 
 const app = express()
 
-const url = ''
+const url = 'https://kr.investing.com/central-banks/'
 
 axios(url)
 .then(response => {
     const html = response.data
     const $ = cheerio.load(html)
-    $('', html).each(function () {
-        $(this).text
+
+    const centralBanks = []
+
+    $('#curr_table > tbody', html).each(function(index, element) {
+        const bank = $(element).find(`tr:nth-child(${index + 1}) > td.bold.left.noWrap > a`).text()
+        const url = $(element).find(`tr:nth-child(${index + 1}) > td.bold.left.noWrap > a`).attr('href')
+        const interest = $(element).find(`tr:nth-child(${index + 1}) > td:nth-child(3)`).text()
+        
+        centralBanks.push({
+            bank,
+            url,
+            interest
+        })
+        //은행 이름(bank)
+        //"#curr_table > tbody > tr:nth-child(2) > td.bold.left.noWrap > a"
+        //"#curr_table > tbody > tr:nth-child(1) > td.bold.left.noWrap > a"
+
+        //url - href
+        //#curr_table > tbody > tr:nth-child(1) > td.bold.left.noWrap > a
+        //#curr_table > tbody > tr:nth-child(2) > td.bold.left.noWrap > a
+
+        //현재 금리(interest)
+        //#curr_table > tbody > tr:nth-child(1) > td:nth-child(3)
+        //#curr_table > tbody > tr:nth-child(2) > td:nth-child(3)
     })
-})
+    console.log(centralBanks)
+
+}).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
