9ddbf22b564f31afc1b77f4ea6e17aed12239117
jjpp43 <junnapark@gmail.com>
2022-01-20 10:29:07 +0000

    Successfully retrived data and saved it in centralBanks list

--- index.js
+++ index.js
@@ -14,28 +14,18 @@ axios(url)
 
     const centralBanks = []
 
-    $('#curr_table > tbody', html).each(function(index, element) {
-        const bank = $(element).find(`tr:nth-child(${index + 1}) > td.bold.left.noWrap > a`).text()
-        const url = $(element).find(`tr:nth-child(${index + 1}) > td.bold.left.noWrap > a`).attr('href')
-        const interest = $(element).find(`tr:nth-child(${index + 1}) > td:nth-child(3)`).text()
+    $('#curr_table > tbody > tr', html).each((index, element) => {
+        if(index <=12) {
+            const bank = $(element).find(`:nth-child(${index + 1}) > td.bold.left.noWrap > a`).text()
+            const url = $(element).find(`:nth-child(${index + 1}) > td.bold.left.noWrap > a`).attr('href')
+            const interest = $(element).find(`:nth-child(${index + 1}) > td:nth-child(3)`).text()
         
-        centralBanks.push({
-            bank,
-            url,
-            interest
-        })
-
-        //은행 이름(bank)
-        //"#curr_table > tbody > tr:nth-child(1) > td.bold.left.noWrap > a"
-        //"#curr_table > tbody > tr:nth-child(2) > td.bold.left.noWrap > a"
-
-        //url - href
-        //#curr_table > tbody > tr:nth-child(1) > td.bold.left.noWrap > a
-        //#curr_table > tbody > tr:nth-child(2) > td.bold.left.noWrap > a
-
-        //현재 금리(interest)
-        //#curr_table > tbody > tr:nth-child(1) > td:nth-child(3)
-        //#curr_table > tbody > tr:nth-child(2) > td:nth-child(3)
+            centralBanks.push({
+                bank,
+                url,
+                interest
+            })
+        }   
     })
     console.log(centralBanks)
 
